2df07afaa653a61f4f2092ca333b9a1df1e21498
Prince San Juan <pasanjuan@gbox.adnu.edu.ph>
2025-11-03 00:31:22 +0800

    Fully Design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Login from "./pages/login";
-import Signup from "./pages/Signup";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Auth from "./pages/Auth";
 import "./App.css";
 
 function App() {
-  console.log("App component is rendering");
-
   return (
     <Router>
-      <div style={{ padding: 20 }}>
-        <nav className="auth-links">
-          <Link to="/login" style={{ marginRight: 10 }}>
-            Login
-          </Link>
-          <Link to="/signup">Signup</Link>
-        </nav>
-
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/login" element={<Auth />} />
+        <Route path="/signup" element={<Auth />} />
+      </Routes>
     </Router>
   );
 }
