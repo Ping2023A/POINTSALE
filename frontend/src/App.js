d8952d18def03cbf27e4547e2ab7c4cb6d46bebe
sdecastro <sdecastro@gbox.adnu.edu.ph>
2025-11-03 23:16:28 +0800

    Inventory Page (frontend only)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Auth from "./pages/Auth";
 import OrderPage from "./pages/OrderPage";
+import Inventory from "./pages/Inventory";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/login" element={<Auth />} />
         <Route path="/signup" element={<Auth />} />
         <Route path="/order" element={<OrderPage />} />
+        <Route path="/inventory" element={<Inventory />} />
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
